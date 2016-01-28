20220e9849f07ccc7a7491250524903aa0e34eb5
michsart <github@mikenet.ca>
2016-01-28 16:38:32 -0500

    201601281638

--- lab1/showsetidfiles.sh
+++ lab1/showsetidfiles.sh
@@ -3,5 +3,7 @@
 # This script will list all of the setuid and setgid regular files in the /usr directory tree
 # Michael Sartori - Jan 22 2016
 
-
-find /usr -name setuid* -o -name setgid*
+echo "setuid:"
+find /usr -perm -4000 -ls
+echo -e "\nsetgid:"
+find /usr -perm -6000 -ls
