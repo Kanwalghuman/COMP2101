5fad850f54b434f71f9ebafa0cdb9ea3516404e1
michsart <github@mikenet.ca>
2016-02-18 01:57:20 -0500

    modified cmdargs.sh

--- lab3/cmdargs.sh
+++ lab3/cmdargs.sh
@@ -3,6 +3,7 @@
 # this script will display all of the command line arguments passed to it at execution, on per line
 # Michael Sartori - Feb 13, 2016
 
-for arg in $@; do
-	echo $arg
+while [ $# -gt 0 ]; do
+	echo $1
+	shift
 done
